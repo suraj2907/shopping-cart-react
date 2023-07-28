fade82f051a0fb967a960257f3ef2aedaf868ef0
Suraj Jawrani <surajjawrani2022@gmail.com>
2023-07-28 20:32:35 +0530

    updated .env File

--- src/FireBaseConfig/FireBaseConfig.js
+++ src/FireBaseConfig/FireBaseConfig.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 const FireBaseConfig = {
-  apiKey: "AIzaSyDmCpxJPEiS8fGLWAn9uaiQrhXJEl2DV2I",
-  authDomain: "shopping-cart-64835.firebaseapp.com",
-  projectId: "shopping-cart-64835",
-  storageBucket: "shopping-cart-64835.appspot.com",
-  messagingSenderId: "74985906209",
-  appId: "1:74985906209:web:30410b8c9cb8357aae4360",
-  measurementId: "G-0X3QX4H5X0",
+  apiKey: `${process.env.REACT_APP_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_APP_ID}`,
+  measurementId: `${process.env.REACT_APP_MEASUREMENT_ID}`,
 };
 
 export default FireBaseConfig;
